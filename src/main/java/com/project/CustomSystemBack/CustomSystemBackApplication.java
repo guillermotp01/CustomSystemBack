dfb95ead398f7d9f77d10e20f0994ab3a24ea125
guillermotp01 <n00247337@gmail.com>
2025-01-26 19:16:11 -0500

    first commit

---
+++ src/main/java/com/project/CustomSystemBack/CustomSystemBackApplication.java
@@ -0,0 +1,13 @@
+package com.project.CustomSystemBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomSystemBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomSystemBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/CustomSystemBack/CustomSystemBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.CustomSystemBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomSystemBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
